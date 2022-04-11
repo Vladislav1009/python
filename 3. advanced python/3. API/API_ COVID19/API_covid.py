28b5b762a9a4afaf719cb6e297460312d1613c0d
Vladislav1009 <Vlados.42_rus@mail.ru>
2022-04-11 15:04:26 +0700

    доделал API_covid.py

--- 3. advanced python/3. API/API_ COVID19/API_covid.py
+++ 3. advanced python/3. API/API_ COVID19/API_covid.py
@@ -6,27 +6,85 @@
 
 #  Ключ --> c784b67935mshd9a7f3cab6995ebp190aafjsnacc724b616ce
 
-
-
-from wsgiref import headers
 import requests
-
+import datetime
+import pandas as pd
 
 class Covid():
 
-    def __init__(self, api_key: str, ):
-        self.api_key = api_key
-        self.url = f"https://covid-193.p.rapidapi.com"
+    def __init__(self, api_key: str, rapidapi: str):
+        """
+        Конструктор класса, который автоматически выводится при создании объектов
 
-    def get_countries(self):
-        headers = {
-            'x-rapidapi-host': self.url,
+        Args:
+            api_key([str], обязательный параметр) - ключ. Берем на apiRapid;
+            rapidapi([str]), обязательный параметр) - ссылка на сайт, надо для использования headers при запросе.
+        """
+        self.api_key = api_key
+        self.url_covid = f'https://covid-193.p.rapidapi.com'
+        self.rapidapi = rapidapi
+        self.headers = {
+            'x-rapidapi-host': self.rapidapi,
             'x-rapidapi-key': self.api_key
         }
-        response = requests.request('GET', f'{self.url}/countries', headers=headers)
-        return print(response.json())
 
+    def get_countries(self, country: str = None):
+        """
+        Выводит список стран затронутых covid_19. Есть возможность не выводить список, а запросить конкретную строку.
 
-API_KEY = 'c784b67935mshd9a7f3cab6995ebp190aafjsnacc724b616ce'
-covid_19 = Covid(api_key = API_KEY)
-countries = covid_19.get_countries()
+        Args:
+            country([country], не обязательный параметр) - если None, то выводится список стран, если не None, тогда проверка конкретной страны
+        """
+        params = {'search': country}
+        response = requests.request('GET', f'{self.url_covid}/countries', headers = self.headers, params = params)
+        return response.json()
+        
+    def get_statistics(self, country:str = None):
+        """
+        Отражает текущий статус распространения коронавируса во всех странах. Можно фильтровать country, чтобы получить его текущий статус.
+
+        Args:
+            country([country], не обязательный параметр) - если None, то выводится список стран, если не None, тогда проверка конкретной страны
+        """
+        params = {'country': country}
+        response = requests.request('GET', f'{self.url_covid}/statistics', headers = self.headers, params = params)
+        return response.json()
+
+    def get_history(self, country: str, date: str = None): # Надо обработать случай, если country будет не указан, сейчас это просто исключение
+        """
+        Запрос всей истории статистики для страны. 
+
+        Args:
+            country([country], обязательный параметр) - название страны;
+            date([], не обязательный) - Дата и время
+        """
+        params = {
+            'country': country,
+            'day': date
+        }
+        response = requests.request('GET', f'{self.url_covid}/history', headers = self.headers, params = params)
+        return response.json()
+
+    def df_func(self, func):
+        """
+        Преобразует результаты функций запроса в Data Frame
+
+        Args:
+            func([Any], oбязательный) - объект class Covid()
+        """  
+        return pd.DataFrame(func.get('response'))
+             
+
+rapidapi_host = 'covid-193.p.rapidapi.com'
+API_KEY = "c784b67935mshd9a7f3cab6995ebp190aafjsnacc724b616ce"
+country = 'USA'
+date = datetime.date(2022, 1, 1).strftime('%Y-%m-%d')
+
+covid_19 = Covid(api_key = API_KEY, rapidapi = rapidapi_host)
+countries_obj = covid_19.get_countries()
+statistics_obj = covid_19.get_statistics()
+history_obj = covid_19.get_history(country)
+df_obj = covid_19.df_func(statistics_obj)
+
+df_obj.to_excel('./draft_api.xlsx', index = False)
+
